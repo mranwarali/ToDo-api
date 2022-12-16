28070f86861ce6b7a00c35b4d11c04562ed76908
Anwar Ali <anwar955732@gmail.com>
2022-12-16 14:46:44 +0530

    first commit

---
+++ todoapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('',ListTodo.as_view()),
+    path('create',CreateTodo.as_view()),
+    path('delete/<int:pk>',DeleteTodo.as_view()),
+    path('update/<int:pk>',UpdateTodo.as_view()),
+]
---
+++ todoapp/views.py
@@ -0,0 +1,19 @@
+from rest_framework import generics
+from .serializers import *
+from .models import *
+
+class ListTodo(generics.ListAPIView):
+    queryset = ToDo.objects.all()
+    serializer_class = ToDoSerializer
+
+class CreateTodo(generics.CreateAPIView):
+    queryset = ToDo.objects.all()
+    serializer_class = ToDoSerializer
+
+class DeleteTodo(generics.DestroyAPIView):
+    queryset = ToDo.objects.all()
+    serializer_class = ToDoSerializer
+
+class UpdateTodo(generics.RetrieveUpdateAPIView):
+    queryset = ToDo.objects.all()
+    serializer_class = ToDoSerializer
